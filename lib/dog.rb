14cdfee460f744579d320fddccf7aff90f55a82a
Seth Near <seth.j.near@gmail.com>
2020-06-23 14:11:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,25 @@ class Dog
 
 @@all = []
 
+attr_reader :name
+
 def initialize(name)
   @name = name
   @@all << self
 end
 
+def self.all
+  return @@all
+end
 
+def self.clear_all
+  @@all.clear
+end
 
+def self.print_all
+  @@all.each {|dog|
+    puts "#{dog.name}"  
+  }
+end
 
 end
